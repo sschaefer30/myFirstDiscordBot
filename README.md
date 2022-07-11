bf770bbaeb4c0070d682f0d32a6f55bf12ac1139
Schaefer, Scott David <scottdavidschaefer@gmail.com>
2022-07-10 20:02:13 -0400

    Text Parsing Updates

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ My first discord bot that I have made.
 DEVNOTES: 
 In CMD in correct directory to start bot: node index.js
 ## Current features:
-- !myWeather <INSERT LOCATION>: using https://www.weatherapi.com/
+- !weather <INSERT LOCATION>: using https://www.weatherapi.com/
 - !triggers, !add trigger <NEW TRIGGER>, !remove trigger <EXISTING TRIGGER>: responds to certain keywords added by users
 
 
--- commands/weather.js
+++ commands/weather.js
@@ -5,9 +5,9 @@ const key = keys.WEATHER_API_KEY
 
 export default function weatherFunc(message) {
     const content = message.content
-    if (content.startsWith("!myWeather ")) {
-        const location = content.slice(11, content.length)
-        const call = "http://api.weatherapi.com/v1/current.json?key=" + key + "&q=" + location + "&aqi=no"
+    if (content.startsWith("!weather ")) {
+        const location = content.slice(9, content.length)
+        const call = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&aqi=no`
         fetch(call)
             .then(response => response.json())
             .then(data => {
@@ -23,7 +23,6 @@ function weatherOut(message, data) {
     }
     const locationData = data.location
     const weatherData = data.current
-    message.channel.send("In " + locationData.name + ", " + locationData.country + ", it is currently "
-       + "**" + weatherData.temp_c + "°C (" + weatherData.temp_f + "°F) and " + weatherData.condition.text + "**" + ".")
-    // New York, United States
+
+    message.channel.send(`In ${locationData.name}, ${locationData.country}, it is currently **${weatherData.temp_c}°C (${weatherData.temp_f} °F)** and **${weatherData.condition.text}**.`)
 }
